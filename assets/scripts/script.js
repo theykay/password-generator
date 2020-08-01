6a73d4f0d528cb8c9b4a6dd327f0a4ed1405c7f9
TheyKay <murraykyleb@gmail.com>
2020-08-01 16:42:33 -0600

    started randomize function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  // print this so you know when this function is called
+  // DELETE BEFORE PUBLISHING
+  console.log('generatePassword is running');
+  let plength = prompt("Password length: ");
+  if (parseInt(plength) === 'number' && 8 <= plength <= 128) {
+    
+  } else {
+    alert('Password must be 8 to 128 characters in length');
+    return;
+  }
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
